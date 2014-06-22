64dd7a8a130dbaf71542a28de5bf8d29398f48f8
Alexander Klaiber <alex.klaiber@gmail.com>
2014-06-22 11:21:33 +0200

    add response class

--- lib/rgcm/response.rb
+++ lib/rgcm/response.rb
@@ -1,5 +1,38 @@
 module Rgcm
   class Response
+    attr_reader :body
+
+    def initialize(json)
+      @body = JSON.parse(json, symbolize_names: true)
+    end
+
+    def count_successes
+      self.body[:success]
+    end
+
+    def has_successes?
+      self.count_successes > 0
+    end
+
+    def count_failures
+      self.body[:failure]
+    end
+
+    def has_failures?
+      self.count_failures > 0
+    end
+
+    def results
+      self.body[:results]
+    end
+
+    def errors
+      self.results.select { |result| result.key?(:error) }
+    end
+
+    def successes
+      self.results.select { |result| !result.key?(:error) }
+    end
 
   end
 end
--- spec/rgcm/response_spec.rb
+++ spec/rgcm/response_spec.rb
@@ -1,5 +1,62 @@
 require 'spec_helper'
 
 describe Rgcm::Response do
+  let(:response) { Rgcm::Response.new(json) }
 
+  context 'with successfully response' do
+    let(:json) { File.open("#{FIXTURES_PATH}/responses/successfully.json").read }
+    it '#has_successes?' do
+      expect(response.has_successes?).to be_true
+    end
+    it '#has_failures?' do
+      expect(response.has_failures?).to be_false
+    end
+    it '#count_successes' do
+      expect(response.count_successes).to eql(1)
+    end
+    it '#has_failures?' do
+      expect(response.count_failures).to eql(0)
+    end
+  end
+
+  context 'with failure response' do
+    let(:json) { File.open("#{FIXTURES_PATH}/responses/failure.json").read }
+    it '#has_successes?' do
+      expect(response.has_successes?).to be_false
+    end
+    it '#has_failures?' do
+      expect(response.has_failures?).to be_true
+    end
+    it '#count_successes' do
+      expect(response.count_successes).to eql(0)
+    end
+    it '#has_failures?' do
+      expect(response.count_failures).to eql(1)
+    end
+    it '#errors' do
+      expect(response.errors.size).to eql(1)
+    end
+  end
+
+  context 'with failure response' do
+    let(:json) { File.open("#{FIXTURES_PATH}/responses/multi.json").read }
+    it '#has_successes?' do
+      expect(response.has_successes?).to be_true
+    end
+    it '#has_failures?' do
+      expect(response.has_failures?).to be_true
+    end
+    it '#count_successes' do
+      expect(response.count_successes).to eql(3)
+    end
+    it '#has_failures?' do
+      expect(response.count_failures).to eql(11)
+    end
+    it '#errors' do
+      expect(response.errors.size).to eql(11)
+    end
+    it '#successes' do
+      expect(response.successes.size).to eql(3)
+    end
+  end
 end
